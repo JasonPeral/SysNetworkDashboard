0605cdfbd79441f6bbcd11ccd822f0deb2f3f0fc
Jason Peral <jasonperal@hotmail.com>
2025-01-07 14:06:28 -0500

    variables added to depency calls

--- system_info_parse/main.py
+++ system_info_parse/main.py
@@ -2,24 +2,34 @@ import platform
 import psutil
 import cpuinfo
 
+
+#moving hardcoded dependency calls into variables for later use with dashboard
+sys_architecture = platform.architecture()
+sys_pc_name = platform.node()
+sys_os = platform.platform
+sys_basic_cpu = platform.processor()
+sys_cpuinfo = cpuinfo.get_cpu_info()
+sys_cpu_bits = sys_cpuinfo['bits']
+sys_total_ram = psutil.virtual_memory().total/1024/1024/1024
+sys_cpu_usage = psutil.cpu_percent(5)
+
 #information on the architecture of our machine 
-print(f"Architecture: {platform.architecture()}")
-print(f"Computer Name: {platform.node()}")
-print(f"OS: {platform.platform}")
+print(f"Architecture: {sys_architecture}")
+print(f"Computer Name: {sys_pc_name}")
+print(f"OS: {sys_os}")
 
 #less detailed info on cpu
-print(f"Processor: {platform.processor()}")
+print(f"Processor: {sys_basic_cpu}")
 
-my_cpuinfo = cpuinfo.get_cpu_info();
-print(f"Full CPU info: {my_cpuinfo['brand_raw']}")
+print(f"Full CPU info: {sys_cpuinfo['brand_raw']}")
 
 #Apple does not display cpu hz information. Opting to run bits key
-print(f"Bits: {my_cpuinfo['bits']}")
+print(f"Bits: {sys_cpu_bits}")
 
 #To see what keys are available to parse in .get_cpu_info
 # print(my_cpuinfo.keys())
 
 #ram info 
-print(f"Total RAM: {psutil.virtual_memory().total/1024/1024/1024}GB")
+print(f"Total RAM: {sys_total_ram}GB")
 #Cpu usuge 
-print(f"CPU usage: {psutil.cpu_percent(5)}")
+print(f"CPU usage: {sys_cpu_usage}")
